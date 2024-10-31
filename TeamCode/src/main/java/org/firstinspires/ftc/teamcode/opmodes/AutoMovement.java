50c9c8e952957bcc14514e9d40645215e26f8fc7
JoshuaShrimp <157165465+JoshuaShrimp@users.noreply.github.com>
2024-10-31 15:40:25 -0400

    Test commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoMovement.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.hardware.Motors;
+
+@Autonomous
+public class AutoMovement extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Motors bobot = new Motors(hardwareMap);
+        waitForStart();
+        bobot.frontLeft.runToPosition(12345678, 1);
+        bobot.frontRight.runToPosition(12345678, 1);
+        bobot.backLeft.runToPosition(12345678, 1);
+        bobot.backRight.runToPosition(12345678, 1);
+        sleep(3500);
+        bobot.frontLeft.runToPosition(12345678, -1);
+        bobot.frontRight.runToPosition(12345678, -1);
+        bobot.backLeft.runToPosition(12345678, -1);
+        bobot.backRight.runToPosition(12345678, -1);
+    }
+}
