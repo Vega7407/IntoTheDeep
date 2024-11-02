edb4d5f7a36edc576014a1c76966efa7f3d61892
daniel yang <daniely1021@gmail.com>
2024-11-02 18:56:27 -0400

    fix hardwaremap issues in slidestuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SlideStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SlideStuff.java
@@ -8,31 +8,36 @@ import org.firstinspires.ftc.teamcode.hardware.Slide;
 
 @TeleOp
 public class SlideStuff extends LinearOpMode {
-    Slide slides = new Slide(hardwareMap);
+    Slide slides;
     DcMotorEx slideMotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        slides = new Slide(hardwareMap);
         slideMotor = hardwareMap.get(DcMotorEx.class, "slideMotor");
 
-        // these three if statements rotate the slide up when dpad up is pressed,
-        // rotate the slide down when dpad down is pressed,
-        // and stop the slide when no button is pressed
-        if (gamepad1.dpad_up) {
-            slideMotor.setPower(1);
-        } else if (gamepad1.dpad_down) {
-            slideMotor.setPower(-.75);
-        } else {
-            slideMotor.setPower(0);
-        }
+        waitForStart();
 
-        // these three if statements turn the motor when dpad up is pressed to extend the slide,
-        // turn the motor the other way when dpad down is pressed to retract the slide,
-        // and stop the slide when no button is pressed
-        if (gamepad1.dpad_right) {
-            slides.extendSlide();
-        } else if (gamepad1.dpad_left) {
-            slides.retractSlide();
+        while (opModeIsActive()) {
+            // these three if statements rotate the slide up when dpad up is pressed,
+            // rotate the slide down when dpad down is pressed,
+            // and stop the slide when no button is pressed
+            if (gamepad1.dpad_up) {
+                slideMotor.setPower(1);
+            } else if (gamepad1.dpad_down) {
+                slideMotor.setPower(-.75);
+            } else {
+                slideMotor.setPower(0);
+            }
+
+            // these three if statements turn the motor when dpad up is pressed to extend the slide,
+            // turn the motor the other way when dpad down is pressed to retract the slide,
+            // and stop the slide when no button is pressed
+            if (gamepad1.dpad_right) {
+                slides.extendSlide();
+            } else if (gamepad1.dpad_left) {
+                slides.retractSlide();
+            }
         }
     }
 }
