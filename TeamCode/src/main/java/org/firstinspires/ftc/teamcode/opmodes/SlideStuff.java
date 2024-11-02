e8d72a917563e78101eef327c78ee67e72d30a23
JoshuaShrimp <157165465+JoshuaShrimp@users.noreply.github.com>
2024-11-02 18:48:11 -0400

    Made slides manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SlideStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SlideStuff.java
@@ -18,21 +18,32 @@ public class SlideStuff extends LinearOpMode {
         // these three if statements rotate the slide up when dpad up is pressed,
         // rotate the slide down when dpad down is pressed,
         // and stop the slide when no button is pressed
-        if (gamepad1.dpad_up) {
-            slideMotor.setPower(1);
-        } else if (gamepad1.dpad_down) {
-            slideMotor.setPower(-.75);
-        } else {
-            slideMotor.setPower(0);
-        }
 
-        // these three if statements turn the motor when dpad up is pressed to extend the slide,
-        // turn the motor the other way when dpad down is pressed to retract the slide,
-        // and stop the slide when no button is pressed
-        if (gamepad1.dpad_right) {
-            slides.extendSlide();
-        } else if (gamepad1.dpad_left) {
-            slides.retractSlide();
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.dpad_up) {
+                slideMotor.setPower(1);
+            } else if (gamepad1.dpad_down) {
+                slideMotor.setPower(-.75);
+            } else {
+                slideMotor.setPower(0);
+            }
+
+            // these three if statements turn the motor when dpad up is pressed to extend the slide,
+            // turn the motor the other way when dpad down is pressed to retract the slide,
+            // and stop the slide when no button is pressed
+            if (gamepad1.dpad_right) {
+                slides.slide.setPower(1.0);
+            } else if (gamepad1.dpad_left) {
+                slides.slide.setPower(-1.0);
+            } else {
+                slides.slide.setPower(0.0);
+            }
+
+            telemetry.addData("current position", slides.slide.getPosition());
+            telemetry.update();
         }
     }
 }
