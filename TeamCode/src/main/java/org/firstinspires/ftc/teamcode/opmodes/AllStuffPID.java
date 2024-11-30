8a2f2d5616b144581ee288c6c3f70382571fb280
daniel yang <daniely1021@gmail.com>
2024-11-30 15:18:17 -0500

    allstuffPID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AllStuffPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AllStuffPID.java
@@ -11,6 +11,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.control.PIDFController;
 import org.firstinspires.ftc.teamcode.hardware.Chassis;
 import org.firstinspires.ftc.teamcode.hardware.Motor;
 import org.firstinspires.ftc.teamcode.hardware.Slide;
@@ -23,23 +24,8 @@ public class AllStuffPID extends LinearOpMode {
     Slide slides;
     Motor slideMotor;
     Chassis bobot;
-    Gamepad lastGamepad1;
-    double target;
-    double position;
-    double error;
-    double power;
-    final int tolerance = 700;
-    boolean doPID = true;
-    double reference;
-
-    double integralSum;
-
-    double lastError;
-
+    PIDFController controller;
     boolean setPointIsNotReached;
-    double out;
-    double derivative;
-    double Kcos;
     @Override
     public void runOpMode() throws InterruptedException {
         claw = new TwoPointServo(0.45, 0.7, "claw", hardwareMap);
@@ -48,19 +34,10 @@ public class AllStuffPID extends LinearOpMode {
         slideMotor = new Motor(hardwareMap.get(DcMotorEx.class, "slideMotor"));
         slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bobot = new Chassis(hardwareMap);
-        lastGamepad1 = new Gamepad();
-        slideMotor.reset();
-
-        reference = 0; // some value
-
-        integralSum = 0;
-
-        lastError = 0;
-        setPointIsNotReached = true;
+        controller = new PIDFController(new PIDFController.PIDCoefficients(1, 0, 0.1));
 
         waitForStart();
 
-        ElapsedTime timer = new ElapsedTime();
 
         while (opModeIsActive()) {
             // these two if statements open the servo when A is pressed and close the servo when B is pressed
@@ -77,35 +54,23 @@ public class AllStuffPID extends LinearOpMode {
                 clawWrist.positionB();
             }
 
-            if (setPointIsNotReached) {
-                // obtain encoder position
-                position = slideMotor.getPosition();
-
-                // calculate the error
-                error = reference - position;
-
-                // rate of change of the error
-                derivative = (error - lastError) / timer.seconds();
-
-                // sum of all error over time
-                integralSum = integralSum + (error * timer.seconds());
-
-                out = (Math.cos(reference) * Kcos) + integralSum + derivative;
-
-                slideMotor.setPower(out);
-
-                lastError = error;
-
-                // reset the timer for next time
-                timer.reset();
-            }
             if (gamepad1.dpad_up) {
-                reference = 45;
+                controller.setTargetPosition((int) (45 * Motor.getTPD_84()));
             } else if (gamepad1.dpad_down) {
-                reference = 0;
-            } else if (gamepad1.right_bumper) {
-                doPID = !doPID;
+                controller.setTargetPosition(0);
             }
+
+            double power = controller.update(slideMotor.getPosition());
+            double error = controller.getTargetPosition() - slideMotor.getPosition();
+
+            telemetry.addData("Target", controller.getTargetPosition());
+            telemetry.addData("Position", slideMotor.getPosition());
+            telemetry.addData("Error", error);
+            telemetry.addData("Power", power);
+            telemetry.update();
+
+            slideMotor.setPower(power);
+
 
             // these three if z statements turn the motor when dpad up is pressed to extend the slide,
             // turn the motor the other way when dpad down is pressed to retract the slide,
@@ -126,15 +91,6 @@ public class AllStuffPID extends LinearOpMode {
             double rx = gamepad1.right_stick_x;
 
             bobot.setMotorPowers(y, x, rx);
-
-            telemetry.addData("Target", target);
-            telemetry.addData("Prop", error/target);
-            telemetry.addData("Error", error);
-            telemetry.addData("Pos", position);
-            telemetry.addData("Pow", power);
-            telemetry.addData("claw", claw.claw.getPosition());
-            telemetry.addData("wrist", clawWrist.claw.getPosition());
-            telemetry.update();
         }
     }
 }
