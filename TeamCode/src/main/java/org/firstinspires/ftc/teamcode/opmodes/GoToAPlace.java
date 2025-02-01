a6d65226669b80c45e04db3123df84c83f8fa0b5
daniel yang <daniely1021@gmail.com>
2025-02-01 13:49:36 -0500

    tuning should be complete, maybe angle needs to be adjusted, add looping function in GoToAPlace to raise and lower arm to set target

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GoToAPlace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GoToAPlace.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import static java.lang.Math.round;
+
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
@@ -7,25 +9,78 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.teamcode.hardware.Chassis;
+import org.firstinspires.ftc.teamcode.hardware.Motor;
+import org.firstinspires.ftc.teamcode.hardware.Slide;
+import org.firstinspires.ftc.teamcode.hardware.TwoPointServo;
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+import dev.frozenmilk.dairy.pasteurized.SDKGamepad;
+import page.j5155.expressway.ftc.motion.FeedforwardFun;
+import page.j5155.expressway.ftc.motion.PIDFController;
 
 @Autonomous
 public class GoToAPlace extends LinearOpMode {
+    TwoPointServo claw;
+    TwoPointServo clawWrist;
+    Motor slideMotor;
+    PIDFController controller;
+    boolean clawToggle;
+    boolean clawWristToggle;
+    public static double p = -0.002, i = 0, d = 0.0001;
+    public static double normalF = 0.01;
+    public static double zeroF = 0.12;
+    public static double fullF = 0.15;
+    public static double f = normalF;
+    //    public static double f = 0.8;
+    public static int target;
+    private final double ticks_in_degree = round(1993.6 / 360);
+    public static PIDFController.PIDCoefficients coefficients = new PIDFController.PIDCoefficients(p,i,d);
+
     @Override
     public void runOpMode() {
+        claw = new TwoPointServo(0.18, 0, 1, "claw", hardwareMap);
+        clawWrist = new TwoPointServo(0.3, 0.6, 0.9, "clawWrist", hardwareMap);
+        claw.positionA();
+        clawWrist.positionB();
+        slideMotor = new Motor(hardwareMap.get(DcMotorEx.class, "slideMotor"));
+        slideMotor.reverse();
+        slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        target = 0;
+        FeedforwardFun armFF = (position, velocity) -> {
+            double distanceFromTop = (Math.abs(position - 650) / 100);
+//            Log.d("VEGAff", "v " + velocity + " p " + position + " factor " + distanceFromTop);
+            if (velocity != null && position > 100) {
+                double ff = (position > 650 ? 1.0 : -1.0) * distanceFromTop * Math.abs(velocity / 1000) * f;
+//                Log.d("VEGAff", "positive case " + ff);
+                return ff;
+            }
+            else
+                return 0;
+        };
+        controller = new PIDFController(coefficients, armFF);
         MecanumDrive robot = new MecanumDrive(hardwareMap, new Pose2d(0.0, 0.0, 0.0));
         Action traj = robot.actionBuilder(robot.localizer.getPose())
-                .strafeTo(new Vector2d(24.0, 24.0))
+                .splineTo(new Vector2d(30, -40), -Math.PI)
                 .build();
-        Action arm = robot.actionBuilder(robot.localizer.getPose())
-                        .strafeTo(new Vector2d(10.0, 30.0))
+        Action firstGrab = robot.actionBuilder(robot.localizer.getPose())
+                        .strafeTo(new Vector2d(10.0, 60.0))
+
                         .build();
-
-
         waitForStart();
-
-        Actions.runBlocking(traj
+        coefficients.setKI(i);
+        coefficients.setKD(d);
+        int armPos = slideMotor.getPosition();
+        double power = controller.update(System.nanoTime(), armPos, slideMotor.getVelocity());
+        slideMotor.setPower(power);
+        f = normalF;
+        target = (350);
+        coefficients.setKP(p);
+        controller.setTargetPosition(target);
+        Actions.runBlocking(firstGrab
 
         );
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -59,7 +59,7 @@ public final class MecanumDrive {
         // TODO: fill in these values based on
         //   see https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html?highlight=imu#physical-hub-mounting
         public RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection =
-                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+                RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
         public RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection =
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
@@ -83,9 +83,9 @@ public final class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.001;
-        public double headingGain = -0.2; // shared with turn
+        public double axialGain = 0.05;
+        public double lateralGain = -0.0001;
+        public double headingGain = 1; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
@@ -226,10 +226,10 @@ public final class MecanumDrive {
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        leftBack = hardwareMap.get(DcMotorEx.class, "backLeft");
+        rightBack = hardwareMap.get(DcMotorEx.class, "backRight");
+        rightFront = hardwareMap.get(DcMotorEx.class, "frontRight");
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -237,7 +237,8 @@ public final class MecanumDrive {
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // TODO: reverse motor directions if needed
-        //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
